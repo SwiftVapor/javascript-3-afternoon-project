1e7158610d66ab23196ad278353765e0ac8a4458
SwiftVapor <jonathongthayne@gmail.com>
2020-09-06 02:40:18 -0600

    Finished Higher-ordre

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter((element, index, array) =>{
+  return element % 2 === 0;
+})
 
 
 
@@ -44,7 +46,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map((ele, ind, arr) => {
+  return ele * 1.07
+});
 
 
 
@@ -63,7 +67,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((acc, curr)=>{
+  return acc + curr
+});
 
 
 
@@ -89,7 +95,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((ele, ind, array)=>{
+  return ele.CP > 200;
+});
 
 
 
@@ -105,10 +113,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 /*
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
-
-let orderTotals // Code here
-
-
+//code here
+let orderTotals = orders.map((ele, ind, arr) => {
+  return ele.price * (ele.tax+1)
+});
 
 ////////// PROBLEM 6 //////////
 
@@ -126,6 +134,17 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
-
+let sortBob = purchases.filter ((ele, ind, arr) => {
+  return ele.owner === "Bob";
+ });
+ 
+ 
+ let bobsTotalRaw = sortBob.map ((ele, ind, arr) => {
+     return ele.price
+ });
+ 
+ 
+ let bobsTotal = bobsTotalRaw.reduce ((acc, ele, ind, arr) => {
+     return acc + ele
+ });
 
